221578580d1e174359a9c5899e66ef9b3ce91294
Jonathan Friedman <jonbenfri@gmail.com>
2022-06-21 21:21:44 -0400

    sorts filenames by class to better show clustering in heatmap

--- l1_metric_heatmap.py
+++ l1_metric_heatmap.py
@@ -2,14 +2,48 @@
 Code for creating a heatmap of samples collection using L1 distance
 """
 import os
-import glob
-import numpy as np
 import argparse
+import re
+import glob
+
+import numpy as np
+import pandas as pd
 
 from models.utils import l1_metric
 import matplotlib.pyplot as plt
 
-def read_data(input_dir, size=256*256):
+def sort_fileslist(fileslist, samplecsv_path, samplecsv):
+    """
+    Use pandas DataFrame to sort files by class
+    """
+    # Import csv as dataframe
+    csv_file = os.path.join(samplecsv_path, samplecsv)
+    df1 = pd.read_csv(csv_file, sep=",")
+
+    # Get the basename of each file
+    fnames = [os.path.basename(fname) for fname in fileslist]
+    # Get the barcode from each file
+    barcodes = [re.search("A[0-9]+",fname)[0] for fname in fnames]
+    # Labels
+    labels = df1["Cancer"].values.tolist()
+
+    # Create an array of the barcodes to match with fileslist
+    dataframe2_arr = list(zip(barcodes, fileslist, labels))
+
+    df2 = pd.DataFrame(data=dataframe2_arr,
+                       columns=["Barcode","Filename","Label"])
+
+    # Sort by Label, then Barcode
+    df2_sorted = df2.sort_values(by=["Label", "Barcode"])
+
+    # Print the index of the first cancer sample
+    print("Index of first cancer sample:",
+            df2_sorted.index[df2_sorted["Label"] == 0].tolist()[0])
+
+    fileslist_sorted = df2_sorted["Filename"].values.tolist()
+    return fileslist_sorted
+
+def read_data(input_dir, samplecsv_path, samplecsv, size=256*256):
     """
     Reads data into numpy array
     """
@@ -17,7 +51,7 @@ def read_data(input_dir, size=256*256):
     # Get list of files from input directory
     fileslist = glob.glob(os.path.join(input_dir,"*.txt"))
     # Sort files list
-    fileslist.sort()
+    fileslist = sort_fileslist(fileslist, samplecsv_path, samplecsv)
 
     # Get number of files
     file_num = len(fileslist)
@@ -29,7 +63,7 @@ def read_data(input_dir, size=256*256):
     for idx in range(file_num):
         data_array[idx,...] = np.loadtxt(fileslist[idx]).ravel()
 
-    return data_array
+    return data_array, fileslist
 
 def generate_l1_matrix(data_array):
     """
@@ -53,6 +87,7 @@ def plot_matrix(matrix, plotdir, plotname):
     ax1.imshow(matrix, cmap='magma')
     fig.savefig(os.path.join(plotdir, plotname))
 
+
 if __name__ == '__main__':
     print("Current directory: " + os.getcwd())
 
@@ -62,6 +97,8 @@ if __name__ == '__main__':
     parser.add_argument("filesdir", help="The files directory to analyze")
     parser.add_argument("plotdir", help="The directory to save plot")
     parser.add_argument("plotname", help="The output file name to save plot")
+    parser.add_argument("samplecsv_path", help="The path to the csv file with labels")
+    parser.add_argument("samplecsv", help="The filename of the csv file with labels")
 
     args = parser.parse_args()
 
@@ -69,10 +106,12 @@ if __name__ == '__main__':
     filesdir = args.filesdir
     plotdir = args.plotdir
     plotname = args.plotname
+    samplecsv_path = args.samplecsv_path
+    samplecsv = args.samplecsv
 
     # Read the data
     print("Reading input data...")
-    data_array = read_data(filesdir)
+    data_array, fileslist = read_data(filesdir, samplecsv_path, samplecsv)
     # Generate the L1 matrix
     print("Generating L1 heatmap...")
     l1_matrix = generate_l1_matrix(data_array)
