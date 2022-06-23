adec2c7b10ff9bc6584ab534eda91a40e753b015
Jonathan Friedman <jonbenfri@gmail.com>
2022-06-22 21:59:03 -0400

    adds function to cluster correlation matrix

--- models/utils.py
+++ models/utils.py
@@ -4,9 +4,11 @@ Helper functions for models
 import os
 
 import numpy as np
+import pandas as pd
 from scipy.special import jn_zeros
 from scipy.io import savemat
-
+import scipy
+import scipy.cluster.hierarchy as sch
 
 def gen_zeromatrix(shape, save_mat=False, save_numpy=False, outdir=""):
     """
@@ -19,7 +21,7 @@ def gen_zeromatrix(shape, save_mat=False, save_numpy=False, outdir=""):
     for idx in range(nthorder):
         zeromatrix[idx,:] = jn_zeros(idx,kzeros)
 
-    if save_mat:   
+    if save_mat:
         # Save to matlab file
         mdic = {"zeromatrix": zeromatrix}
         filename = "zeromatrix.mat"
@@ -53,3 +55,33 @@ def l1_metric_normalized(A, B):
     A_vec = A.ravel()/np.sum(A)
     B_vec = B.ravel()/np.sum(B)
     return 1*np.sum(abs(A_vec-B_vec))
+
+def cluster_corr(corr_array, inplace=False):
+    # via: https://wil.yegelwel.com/cluster-correlation-matrix/
+    """
+    Rearranges the correlation matrix, corr_array, so that groups of highly
+    correlated variables are next to eachother
+
+    Parameters
+    ----------
+    corr_array : pandas.DataFrame or numpy.ndarray
+        a NxN correlation matrix
+
+    Returns
+    -------
+    pandas.DataFrame or numpy.ndarray
+        a NxN correlation matrix with the columns and rows rearranged
+    """
+    pairwise_distances = sch.distance.pdist(corr_array)
+    linkage = sch.linkage(pairwise_distances, method='complete')
+    cluster_distance_threshold = pairwise_distances.max()/2
+    idx_to_cluster_array = sch.fcluster(linkage, cluster_distance_threshold,
+                                        criterion='distance')
+    idx = np.argsort(idx_to_cluster_array)
+
+    if not inplace:
+        corr_array = corr_array.copy()
+
+    if isinstance(corr_array, pd.DataFrame):
+        return corr_array.iloc[idx, :].T.iloc[idx, :]
+    return corr_array[idx, :][:, idx]
