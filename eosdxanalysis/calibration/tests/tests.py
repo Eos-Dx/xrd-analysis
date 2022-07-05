c5ca6830f54697607878ca25566919e47748543d
Jonathan Friedman <jonbenfri@gmail.com>
2022-07-05 11:16:21 -0400

    starts adding code for generating synthetic silver behenate test images

---
+++ eosdxanalysis/calibration/tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+
+TEST_IMAGE_DIR = os.path.join("eosdxanalysis","calibration","tests","test_images")
+
+
+class TestCalibration(unittest.TestCase):
+    """
+    Test Calibration class
+    """
+
+    def test_synthetic_silver_behenate_images(self):
+        self.fail("Finish test")
+
+
+if __name__ == '__main__':
+    unittest.main()
