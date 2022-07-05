32daee0e93333333ee6c7df73cca110528998ee8
Jonathan Friedman <jonbenfri@gmail.com>
2022-07-05 11:16:21 -0400

    starts adding code for generating synthetic silver behenate test images

---
+++ eosdxanalysis/calibration/tests/test_images/silver_benehate_test_images/gen_test_images.py
@@ -0,0 +1,15 @@
+"""
+Script for generating test images
+"""
+import numpy as np
+
+# Set input directory
+INPUT_DIR="input"
+OUTPUT_DIR="output"
+
+# Set up test image parameters
+h=256
+w=256
+min_val=0
+max_val=255
+
