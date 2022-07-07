6b8035d093bde2cc4df85a01a1e96245dedf0aae
Jonathan Friedman <jonbenfri@gmail.com>
2022-07-07 17:59:16 -0400

    updates calibration materials data structure to allow doublet averaging

--- eosdxanalysis/calibration/materials.py
+++ eosdxanalysis/calibration/materials.py
@@ -5,19 +5,23 @@ Store calibration materials data
 # AgBH q-peaks via:
 # http://gisaxs.com/index.php/Material:Silver_behenate 
 q_peaks_ref_dict = {
-        "silver_behenate": [
-            0.1076,
-            0.2152,
-            0.3228,
-            0.4304,
-            0.5380,
-            0.6456,
-            0.7532,
-            0.8608,
-            0.9684,
-            1.076,
-            1.184,
-            1.369,
-            1.387,
-            ],
+        "silver_behenate": {
+            "singlets": [
+                0.1076,
+                0.2152,
+                0.3228,
+                0.4304,
+                0.5380,
+                0.6456,
+                0.7532,
+                0.8608,
+                0.9684,
+                1.076,
+                1.184,
+                ],
+            "doublets": [
+                1.369,
+                1.387,
+                ],
         }
+}
