41979208338093f85ccb5670917c68804a15de0b
Jonathan Friedman <jonbenfri@gmail.com>
2022-07-20 17:46:38 -0400

    adds vectorized version of rmatrix_SpaceLimited

---
+++ polar_discrete_fourier_transform/rmatrix_SpaceLimited_vectorized.m
@@ -0,0 +1,18 @@
+%% A-2. r matrix for space limited function
+% N1 sample size in radial direction
+% N2 sample size in angular direction
+% R effective space limit
+% zeromatrix is precalculated Bessel zeros
+function r=rmatrix_SpaceLimited(N2,N1,R,zeromatrix)
+	M=(N2-1)/2;
+	iirange = 1:N2;
+	p=iirange-1-M;
+	k=1:N1-1;
+
+	zero2_combined = zeromatrix(end-abs(p),1:N1);
+
+	jpk=zero2_combined(:, k);
+	jpN1=zero2_combined(:, end);
+
+	r(iirange,k)=(jpk./jpN1)*R;
+end
