fixes incorrect scaling for plotting Baddour grid on filtered image in Cartesian spatial space

examples/polar_dft/polar_dft_keratin_xrd.py
@@ -23,8 +23,8 @@ MODULE_PATH = os.path.dirname(__file__)
 DATA_DIR = "data"
 DATA_FILENAME = "CRQF_A00823.txt"
 
-N1 = 100
-N2 = 101
+N1 = 150 # Radial sampling count
+N2 = 151 # Angular sampling count, must be odd
 R = 90
 
 image_path = os.path.join(MODULE_PATH, DATA_DIR, DATA_FILENAME)
@@ -43,21 +43,16 @@ plt.title("Wiener Filtered of Original Cartesian sampling of {}".format(DATA_FIL
 
 # Now take 2D FFT to compare filtering
 
-plt.show()
-
-exit(0)
-
 # Sample our image according on the Baddour grid
 dx = 0.2
 dy = 0.2
 
 # Let's create a meshgrid,
 # note that x and y have even length
-x = np.arange(-R+dx/2, R+dx/2, dx)
-y = np.arange(-R+dx/2, R+dx/2, dy)
-XX, YY = np.meshgrid(x, y)
-
-RR = np.sqrt(XX**2 + YY**2)
+#x = np.arange(-R+dx/2, R+dx/2, dx)
+#y = np.arange(-R+dx/2, R+dx/2, dy)
+#XX, YY = np.meshgrid(x, y)
+#RR = np.sqrt(XX**2 + YY**2)
 
 origin = (image.shape[0]/2-0.5, image.shape[1]/2-0.5)
 
@@ -65,8 +60,8 @@ origin = (image.shape[0]/2-0.5, image.shape[1]/2-0.5)
 # First get rmatrix and thetamatrix
 rmatrix, thetamatrix = sampling_grid(N1, N2, R)
 # Now convert rmatrix to Cartesian coordinates
-Xcart = rmatrix*np.cos(thetamatrix)/dx
-Ycart = rmatrix*np.sin(thetamatrix)/dy
+Xcart = rmatrix*np.cos(thetamatrix)
+Ycart = rmatrix*np.sin(thetamatrix)
 # Now convert Cartesian coordinates to the array notation
 # by shifting according to the origin
 Xindices = Xcart + origin[0]
@@ -74,14 +69,27 @@ Yindices = origin[1] - Ycart
 
 cart_sampling_indices = [Yindices, Xindices]
 
-fdiscrete = map_coordinates(image, cart_sampling_indices)
+fdiscrete = map_coordinates(filtered_img, cart_sampling_indices)
+
+# Plot the original image with the Baddour polar sampling grid in Cartesian coordinates
+fig = plt.figure()
+plt.imshow(filtered_img)
+plt.scatter(Xindices, Yindices, s=1.0)
+plt.title("Filtered image with polar DFT sampling grid")
+
+# plt.show()
+
+# exit(0)
+
 
 t1 = time.time()
 
 print("Start-up time to sample Baddour polar grid:",np.round(t1-t0, decimals=2), "s")
 
-# Calculate the polar dft
+# Calculate the polar dft in frequency space (rho, theta)
 pdft = pfft2_SpaceLimited(fdiscrete, N1, N2, R)
+
+# Convert the polar dft to Cartesian frequency space
 
 t2 = time.time()
 
@@ -89,39 +97,23 @@ print("Time to calculate the polar transform:", np.round(t2-t1, decimals=2), "s"
 
 
 # Warped
-polar_image = warp_polar(image)
+polar_image = warp_polar(filtered_img)
 fig = plt.figure()
 plt.imshow(polar_image)
-plt.title("Polar warped sampling of {}".format(DATA_FILENAME))
+plt.title("Polar warped sampling of filtered {}".format(DATA_FILENAME))
+plt.colorbar()
 
 # Polar DFT
 fig = plt.figure()
-#plt.imshow(20*np.log10(np.abs(pdft)))
-#plt.title("Magnitude DFT of {} [dB]\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
-plt.imshow(np.abs(pdft))
-plt.title("Magnitude DFT of {}\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
-
-fig = plt.figure()
-#plt.imshow(20*np.log10(np.real(pdft)))
-#plt.title("Real part of DFT of {}\n [dB] in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
-plt.imshow(np.real(pdft))
-plt.title("Real part of DFT of {}\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
-
-fig = plt.figure()
-#plt.imshow(20*np.log10(np.imag(pdft)))
-#plt.title("Imaginary part of DFT of {} [dB]\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
-plt.imshow(np.imag(pdft))
-plt.title("Imaginary part of DFT of {}\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
-
-fig = plt.figure()
-plt.imshow(np.angle(pdft))
-plt.title("Phase of DFT of {}\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
+plt.imshow(20*np.log10(np.abs(pdft)))
+plt.title("Magnitude DFT of filtered {} [dB]\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
+plt.colorbar()
 
 # Compare to 2D FFT
-fft = np.fft.fftshift(np.fft.fft2(image))
+fft = np.fft.fftshift(np.fft.fft2(filtered_img))
 
 fig = plt.figure()
 plt.imshow(20*np.log10(np.abs(fft)))
-plt.title("Magnitude FFT of {}\n [dB] in frequency domain".format(DATA_FILENAME))
+plt.title("Magnitude FFT of filtered {}\n [dB] in frequency domain".format(DATA_FILENAME))
 
 plt.show()