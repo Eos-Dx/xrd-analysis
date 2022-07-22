24ec480d65cad0d34337cdcfbdcf50a50c14b810
Jonathan Friedman <jonbenfri@gmail.com>
2022-07-22 19:27:39 -0400

    improves plotting

--- examples/polar_dft/polar_dft_radial_J_1.py
+++ examples/polar_dft/polar_dft_radial_J_1.py
@@ -14,6 +14,8 @@ import matplotlib.pyplot as plt
 
 from eosdxanalysis.models.fourier_analysis import pfft2_SpaceLimited
 from eosdxanalysis.models.polar_sampling import sampling_grid
+from eosdxanalysis.models.polar_sampling import freq_sampling_grid
+from eosdxanalysis.preprocessing.image_processing import unwarp_polar
 
 t0 = time.time()
 
@@ -40,27 +42,18 @@ XX, YY = np.meshgrid(x, y)
 RR = np.sqrt(XX**2 + YY**2)
 TT = np.arctan2(YY, XX)
 
-image = jv(1, RR)*np.cos(TT)
-
-origin = (image.shape[0]/2-0.5, image.shape[1]/2-0.5)
-
-fig = plt.figure()
-plt.imshow(image)
-plt.show()
+n=20
+image = jv(n, RR)*np.cos(TT)
 
 # Sample our image according on the Baddour grid
 dx = 0.2
 dy = 0.2
 
-# Let's create a meshgrid,
-# note that x and y have even length
-x = np.arange(-R+dx/2, R+dx/2, dx)
-y = np.arange(-R+dx/2, R+dx/2, dy)
-XX, YY = np.meshgrid(x, y)
-
-RR = np.sqrt(XX**2 + YY**2)
-
 origin = (image.shape[0]/2-0.5, image.shape[1]/2-0.5)
+
+"""
+Baddour polar grid sampling
+"""
 
 # Now sample the discrete image according to the Baddour polar grid
 # First get rmatrix and thetamatrix
@@ -81,6 +74,10 @@ t1 = time.time()
 
 print("Start-up time to sample Baddour polar grid:",np.round(t1-t0, decimals=2), "s")
 
+"""
+Polar DFT
+"""
+
 # Calculate the polar dft
 pdft = pfft2_SpaceLimited(fdiscrete, N1, N2, R)
 
@@ -88,10 +85,30 @@ t2 = time.time()
 
 print("Time to calculate the polar transform:", np.round(t2-t1, decimals=2), "s")
 
+# Plot the polar DFT on the sample grid in Cartesian frequency space
+
+# Get rhomatrix and psimatrix
+rhomatrix, psimatrix = freq_sampling_grid(N1, N2, R)
+
+FXX = rhomatrix*np.cos(psimatrix)/dx
+FYY = rhomatrix*np.sin(psimatrix)/dy
+
+FXXindices = FXX + origin[0]/R
+FYYindices = origin[1]/R - FYY
+
+F_sampling_indices = [FYYindices, FXXindices]
+
+pdft_cart = map_coordinates(pdft, F_sampling_indices)
+
+
+"""
+Plots
+"""
 
 # Original image
 fig = plt.figure()
 plt.imshow(image)
+plt.scatter(Xindices, Yindices, s=1.0)
 plt.title("Original Cartesian sampling of radial J_1")
 
 # Warped
@@ -107,21 +124,15 @@ fig = plt.figure()
 plt.imshow(np.abs(pdft))
 plt.title("Magnitude DFT of radial J_1\n in polar frequency domain using Baddour coordinates")
 
+# Polar DFT in cartesian frequency domain
 fig = plt.figure()
-#plt.imshow(20*np.log10(np.real(pdft)))
-#plt.title("Real part of DFT of {}\n [dB] in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
-plt.imshow(np.real(pdft))
-plt.title("Real part of DFT of radial J_1{}\n in polar frequency domain using Baddour coordinates")
+plt.imshow(np.abs(pdft_cart))
+plt.title("Magnitude DFT of radial J_1\n in cartesian frequency domain using Baddour coordinates")
 
+# Polar DFT using unwarp_polar
 fig = plt.figure()
-#plt.imshow(20*np.log10(np.imag(pdft)))
-#plt.title("Imaginary part of DFT of {} [dB]\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
-plt.imshow(np.imag(pdft))
-plt.title("Imaginary part of DFT of radial J_1\n in polar frequency domain using Baddour coordinates")
-
-fig = plt.figure()
-plt.imshow(np.angle(pdft))
-plt.title("Phase of DFT of {}\n in polar frequency domain using Baddour coordinates".format(DATA_FILENAME))
+plt.imshow(unwarp_polar(np.abs(pdft).T))
+plt.title("Magnitude DFT of radial J_1\n in cartesian frequency domain using unwarp_polar")
 
 # Compare to 2D FFT
 fft = np.fft.fftshift(np.fft.fft2(image))
@@ -129,5 +140,6 @@ fft = np.fft.fftshift(np.fft.fft2(image))
 fig = plt.figure()
 plt.imshow(20*np.log10(np.abs(fft)))
 plt.title("Magnitude FFT of {}\n [dB] in frequency domain".format(DATA_FILENAME))
+
 
 plt.show()
