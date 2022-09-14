fadbc4198e8e0107d7b0fa30a4b18dc585f0566e
Jonathan Friedman <jonbenfri@gmail.com>
2022-09-14 13:51:35 -0400

    adds code to calculate gaussian decomposition for example

--- examples/gaussian_fit.py
+++ examples/gaussian_fit.py
@@ -28,6 +28,7 @@ from skimage.feature import peak_local_max
 from eosdxanalysis.models.utils import cart2pol
 from eosdxanalysis.models.feature_engineering import EngineeredFeatures
 from eosdxanalysis.models.curve_fitting import GaussianDecomposition
+from eosdxanalysis.models.curve_fitting import gaussian_decomposition
 from eosdxanalysis.preprocessing.utils import create_circular_mask
 from eosdxanalysis.simulations.utils import feature_pixel_location
 
@@ -37,11 +38,24 @@ cmap="hot"
 
 # 1 Run Gaussian decomposition on data set
 # ----------------------------------------
+run_gauss_decomp = True
+if run_gauss_decomp:
+    # Set the input path
+    input_path = ""
+    output_path = ""
 
+    # Run Gaussian decomposition
+    gaussian_decomposition(input_path, output_path)
 
 # 2 Run K-means on Gaussian parameters
 # ------------------------------------
+run_kmeans = False
+if run_kmeans:
+    pass
 
 
 # 3 Run logistic regression on Gaussian parameters
 # ------------------------------------------------
+run_logreg = False
+if run_logreg:
+    pass
