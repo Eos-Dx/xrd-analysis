bd9b320d20faf29214fc982933bfd6f65f9beaa9
Jonathan Friedman <jonbenfri@gmail.com>
2022-09-20 16:33:31 -0400

    uses cross validation on randomly split training and test sets

--- examples/shoulder_analysis.py
+++ examples/shoulder_analysis.py
@@ -14,6 +14,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
@@ -89,16 +91,16 @@ def main(
 
         print("There are " + str(csv_num) + " unique samples.")
 
-        # Y = np.zeros((X.shape[0],1))
-        # Y = df_train['Cancer'].values.reshape((-1,1))
-        # print(Y.shape)
+        # y = np.zeros((X.shape[0],1))
+        # y = df_train['Cancer'].values.reshape((-1,1))
+        # print(..shape)
 
         # Labels
-        Y = np.zeros((csv_num,1),dtype=bool)
+        y = np.zeros((csv_num,1),dtype=bool)
         X_new = np.zeros((csv_num,2))
 
         # Loop over each sample
-        # and average X and label Y
+        # and average X and label y
 
         for idx in np.arange(csv_num):
             # Get a sample
@@ -110,110 +112,44 @@ def main(
             # Now average across all samples
             X_new[idx,:] = np.mean(X[indices,:],axis=0)
             # Get the labels for the samples, first one is ok'
-            Y[idx] = df_train_rows["Cancer"][indices[0]]
+            y[idx] = df_train_rows["Cancer"][indices[0]]
 
 
         X = X_new
         print("Total data count after averaging:")
-        print(Y.shape)
+        print(y.shape)
 
         print("Normal data count:")
-        print(np.sum(Y == False))
+        print(np.sum(y == False))
         print("Cancer data count:")
-        print(np.sum(Y == True))
+        print(np.sum(y == True))
 
     # No patient averaging
     elif not patient_averaging:
-        Y = df_train["Cancer"]
+        y = df_train["Cancer"]
 
-    # Check that X and Y have same number of rows
-    assert(np.array_equal(X.shape[0], Y.shape[0]))
+    # Check that X and y have same number of rows
+    assert(np.array_equal(X.shape[0], y.shape[0]))
+
+    # Randomly split up training and test set
+    X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.4, random_state=0)
 
     # Perform Logistic Regression
     # ---------------------------
-
 
     # Perform logistic regression
     logreg = LogisticRegression(
             C=1e3,class_weight="balanced", solver="newton-cg",
             max_iter=max_iter)
     pipe = Pipeline([('scaler', StandardScaler()), ('logreg', logreg)])
-    pipe.fit(X, Y)
-    print("Score:")
-    print("{:.2f}".format(pipe.score(X,Y)))
+    pipe.fit(X_train, y_train)
 
-    thetas = pipe['logreg'].coef_.ravel()
-    theta0 = pipe['logreg'].intercept_[0]
-    theta_array = np.array([[theta0, *thetas]])
-    # print("Theta array:")
-    # print(theta_array)
+    scores = cross_val_score(pipe, X, y, cv=5)
 
-    # Predict
-    Y_predict = pipe.predict(X)
+    print(scores)
 
-    # Get scores
-    precision = precision_score(Y, Y_predict)
-    print("Precision:")
-    print("{:2.2}".format(precision))
-    recall = recall_score(Y, Y_predict)
-    print("Recall (Sensitivity):")
-    print("{:2.2}".format(recall))
-    # False positive rate: false positives
-    # Of samples identified as positive, what percentage are false
-    print("False positive rate:")
-    false_positives = np.sum(Y[Y_predict == True] == False)
-    predicted_positives = np.sum(Y_predict == True)
-    false_positive_rate = false_positives/predicted_positives
-    print("{:2.2}".format(false_positive_rate))
-
-    # Accuracy = number of correct predictions / total predictions
-    # Balanced accuracy score, weights by counts
-    balanced_accuracy = balanced_accuracy_score(Y, Y_predict)
-    print("Balanced accuracy:")
-    print("{:2.2}".format(balanced_accuracy))
-    # Unbalanced accuracy
-    unbalanced_accuracy = accuracy_score(Y, Y_predict)
-    print("Unbalanced accuracy:")
-    print("{:2.2}".format(unbalanced_accuracy))
-
-    # Test data
-    # ---------
-    if test_data_filepath:
-        # Run test data through trained model and assess performance
-        # Load dataframe
-        df_test = pd.read_csv(test_data_filepath, index_col=0)
-
-        X_test_linear = df_test[[*feature_list]].astype(float).values
-        # Create a polynomial
-        poly = PolynomialFeatures(degree=degree)
-
-        X_test_poly = poly.fit_transform(X_test_linear)
-
-        # Predict
-        Y_predict = pipe.predict(X_test_poly)
-
-        # Save results
-        df_test["Prediction"] = Y_predict
-
-        # Prefix
-        output_prefix = "test_set_predictions"
-
-        # Set output path with a timestamp if not specified
-        if not output_path:
-            # Set timestamp
-            timestr = "%Y%m%dT%H%M%S.%f"
-            timestamp = datetime.utcnow().strftime(timestr)
-
-            output_dir = "predictions_".format(timestamp)
-            output_path = os.path.dirname(training_data_filepath)
-
-        csv_filename = "{}_degree_{}_{}.csv".format(
-                output_prefix, str(degree), timestamp)
-        csv_output_path = os.path.join(output_path, csv_filename)
-
-        df_test["Prediction"].to_csv(csv_output_path)
-
-    # TODO: Save model
+    return scores
 
 if __name__ == '__main__':
     """
