7d2060885f45cc7ac5f9182a2c09226755237d3f
Jonathan Friedman <jonbenfri@gmail.com>
2022-09-20 19:43:19 -0400

    fixes syntax error

--- examples/shoulder_analysis.py
+++ examples/shoulder_analysis.py
@@ -139,7 +139,7 @@ def main(
         X_train, X_test, y_train, y_test = train_test_split(
                 X, y, test_size=0.4, random_state=0)
     else:
-        X_train = X, y_train = y
+        X_train, y_train = X, y
 
     # Perform Logistic Regression
     # ---------------------------
