63ea3fa5b54e2c81e3c996e877ac8bfaf1603e4c
Jonathan Friedman <jonbenfri@gmail.com>
2022-10-05 15:18:57 -0400

    updates code comments

--- eosdxanalysis/preprocessing/angle_finding.py
+++ eosdxanalysis/preprocessing/angle_finding.py
@@ -10,8 +10,9 @@ from scipy.optimize import minimize
 
 def find_rotation_angle(image, r, width, height):
     """
-    Finds the rotation angle of an image based on
-    rectangles in the 9 A region of interest.
+    Finds the rotation angle of an image based on rectangles in the 9 A
+    region of interest. The result is the angle that the image was rotated
+    by to maximize the intensity in the rectangle rois.
 
     Parameters
     ----------
@@ -37,17 +38,68 @@ def find_rotation_angle(image, r, width, height):
     result = minimize(
             rotated_rectangle_roi_sum,
             theta_init,
-            args = (-image, r, width, height))
+            args = (-image, r, width, height),
+            )
     theta = result.x[0]
 
     return theta
 
 def rotated_rectangle_roi_sum(angle, image, r, width, height):
+    """
+    Rotates an image and calculates the total intensity in the
+    rectangle rois.
+
+    Parameters
+    ----------
+
+    angle : float (degrees)
+        The angle to rotate the image by
+
+    image : ndarray
+
+    r : int
+        center of the rectangle from the origin
+
+    width : int
+        horizontal size of the rectangle
+
+    height : int
+        vertical size of the rectangle
+
+    Returns
+    -------
+
+    roi_sum : float
+
+    """
     rotated_image = rotate(image, angle[0])
     roi_sum = rectangle_roi_sum(rotated_image, r, width, height)
     return roi_sum
 
 def rectangle_roi_sum(image, r, width, height):
+    """
+    Calculates the total intensity in the rectangle rois.
+
+    Parameters
+    ----------
+
+    image : ndarray
+
+    r : int
+        center of the rectangle from the origin
+
+    width : int
+        horizontal size of the rectangle
+
+    height : int
+        vertical size of the rectangle
+
+    Returns
+    -------
+
+    roi_sum : float
+
+    """
     shape = image.shape
     center = (shape[0]/2-0.5, shape[1]/2-0.5)
 
