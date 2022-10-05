eec5123137df9cd59c5ed1898cd7966cefeb716d
Jonathan Friedman <jonbenfri@gmail.com>
2022-10-05 10:52:04 -0400

    adds rotation angle finding function based on rectangular rois and associated test

---
+++ eosdxanalysis/preprocessing/angle_finding.py
@@ -0,0 +1,78 @@
+"""
+Code to find the rotation angle for image registration.
+"""
+
+import numpy as np
+
+from skimage.transform import rotate
+from scipy.optimize import minimize
+
+
+def find_rotation_angle(image, r, width, height):
+    """
+    Finds the rotation angle of an image based on
+    rectangles in the 9 A region of interest.
+
+    Parameters
+    ----------
+
+    image : ndarray
+
+    r : int
+        center of the rectangle from the origin
+
+    width : int
+        horizontal size of the rectangle
+
+    height : int
+        vertical size of the rectangle
+
+    Returns
+    -------
+
+    theta : float (degrees)
+
+    """
+    theta_init = 0
+    result = minimize(
+            rotated_rectangle_roi_sum,
+            theta_init,
+            args = (-image, r, width, height))
+    theta = result.x[0]
+
+    return theta
+
+def rotated_rectangle_roi_sum(angle, image, r, width, height):
+    rotated_image = rotate(image, angle[0])
+    roi_sum = rectangle_roi_sum(rotated_image, r, width, height)
+    return roi_sum
+
+def rectangle_roi_sum(image, r, width, height):
+    shape = image.shape
+    center = (shape[0]/2-0.5, shape[1]/2-0.5)
+
+    # Define the right rectangular roi
+    right_row_start = int(center[0] - height/2)
+    right_row_end = int(center[0] + height/2)
+    right_col_start = int((r + center[1]) - width/2)
+    right_col_end = int((r + center[1]) + width/2)
+
+    right_rectangle_rows = slice(right_row_start, right_row_end)
+    right_rectangle_cols = slice(right_col_start, right_col_end)
+
+    right_rectangle_sum = image[right_rectangle_rows, right_rectangle_cols].sum()
+
+    # Define the left rectangular roi
+    left_row_start = int(center[0] - height/2)
+    left_row_end = int(center[0] + height/2)
+    left_col_start = int((center[1] - r) - width/2)
+    left_col_end = int((center[1] - r) + width/2)
+
+    left_rectangle_rows = slice(left_row_start, left_row_end)
+    left_rectangle_cols = slice(left_col_start, left_col_end)
+
+    left_rectangle_sum = image[left_rectangle_rows, left_rectangle_cols].sum()
+
+    rectangle_sum = right_rectangle_sum + left_rectangle_sum
+
+    return rectangle_sum
