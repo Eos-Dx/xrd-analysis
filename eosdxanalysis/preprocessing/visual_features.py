bb3734f3aa0760373b00867b9335bd663e2b3745
Jonathan Friedman <jonbenfri@gmail.com>
2022-11-15 14:15:29 -0500

    removes unused imports

--- eosdxanalysis/preprocessing/visual_features.py
+++ eosdxanalysis/preprocessing/visual_features.py
@@ -1,37 +1,5 @@
-import os
-import glob
-import argparse
-import json
-from datetime import datetime
-import subprocess
-
 import numpy as np
-import pandas as pd
 
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import matplotlib.colors as colors
-
-from scipy import ndimage
-from scipy.ndimage import gaussian_filter
-from skimage.feature import peak_local_max
-from skimage.filters import threshold_local
-from skimage.transform import EuclideanTransform
-from skimage.transform import warp
-from skimage.transform import rotate
-
-from eosdxanalysis.preprocessing.center_finding import find_center
-from eosdxanalysis.preprocessing.center_finding import find_centroid
-from eosdxanalysis.preprocessing.utils import create_circular_mask
-from eosdxanalysis.preprocessing.utils import get_angle
-from eosdxanalysis.preprocessing.utils import find_maxima
-from eosdxanalysis.preprocessing.denoising import find_hot_spots
-from eosdxanalysis.preprocessing.denoising import filter_hot_spots
-from eosdxanalysis.preprocessing.image_processing import crop_image
-from eosdxanalysis.preprocessing.image_processing import quadrant_fold
-from eosdxanalysis.preprocessing.beam_utils import beam_extent
-
-from eosdxanalysis.simulations.utils import feature_pixel_location
 
 def dia_features_global(data):
     """
