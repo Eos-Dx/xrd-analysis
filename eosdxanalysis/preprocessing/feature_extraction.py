2628cbf2dc6239e8db1ca81cf132ef98659c289c
Jonathan Friedman <jonbenfri@gmail.com>
2022-11-15 15:23:13 -0500

    adds annulus intensity function with angstrom arguments

--- eosdxanalysis/preprocessing/feature_extraction.py
+++ eosdxanalysis/preprocessing/feature_extraction.py
@@ -64,20 +64,79 @@ class FeatureExtraction(object):
         Parameters
         ----------
 
-        image : ndarray
-            The measurement image data
+        center : 2-tuple (number)
+            Row and column location of the annulus center
+
+        rmin : number
+            Minimum annulus radius in pixel units
+
+        rmax : number
+            Maximum annulus radius in pixel units
 
         Output
         ------
 
-        intensity : number
-            The total intensity
+        annulus_intensity : number
+            The intensity of the annulus
         """
         # Reference the stored image
         image = self.image
 
         # Get the image shape
         shape = image.shape
+
+        # Create a mask for the annulus
+        annulus_mask = create_circular_mask(shape[0], shape[1], center=center,
+                rmin=rmin, rmax=rmax)
+
+        # Compute the annulus intensity
+        annulus_intensity = np.sum(image[annulus_mask])
+
+        return annulus_intensity
+
+    def feature_annulus_intensity_angstrom(
+            self, distance=None, center=None, amin=None, mmax=None):
+        """
+        Calculate the intensity of an annulus specified by start and end radii
+        in Angstrom units.
+
+        Parameters
+        ----------
+
+        distance : number
+            Distance from sample to detector
+
+        center : 2-tuple (number)
+            Row and column location of the annulus center
+
+        amin : number
+            Minimum annulus angstrom in pixel units
+
+        amax : number
+            Maximum annulus angstrom in pixel units
+
+        Output
+        ------
+
+        annulus_intensity : number
+            The intensity of the annulus
+
+
+        Notes
+        -----
+
+        ``rmin`` and ``rmax`` are swapped from ``amin`` and ``amax`` since we
+        are in reciprocal space.
+
+        """
+        # Reference the stored image
+        image = self.image
+
+        # Get the image shape
+        shape = image.shape
+
+        rmin = somefunction(amax)
+        rmax = somefunction(amin)
 
         # Create a mask for the annulus
         annulus_mask = create_circular_mask(shape[0], shape[1], center=center,
