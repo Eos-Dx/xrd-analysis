ba0383289b64bb1dea2c1f99701fc70ab130562b
Jonathan Friedman <jonbenfri@gmail.com>
2022-11-16 14:01:03 -0500

    adds sectory intensity function with sector specified by minimum and maximum angstroms

--- eosdxanalysis/preprocessing/feature_extraction.py
+++ eosdxanalysis/preprocessing/feature_extraction.py
@@ -251,3 +251,73 @@ class FeatureExtraction(object):
         sector_intensity = np.sum(annulus_mask[sector_indices])
 
         return sector_intensity
+
+    def feature_sector_intensity_angstroms(
+            self, center=None, amin=None, amax=None, theta_min=-np.pi/4,
+            theta_max=np.pi/4):
+        """
+        Computes the total intensity of a sector
+
+        Parameters
+        ----------
+
+        center : 2-tuple (number)
+            Row and column location of the annulus center
+
+        amin : number
+            Minimum annulus radius in pixel units
+
+        amax : number
+            Maximum annulus radius in pixel units
+
+        theta_min : number
+            Start angle of sector in radians. 0 degrees is x > 0 axis. The
+            direction of positive theta is counter-clockwise.
+
+        theta_max : number
+            End angle of sector in radians. 0 degrees is x > 0 axis. The
+            direction of positive theta is counter-clockwise.
+
+        Output
+        ------
+
+        sector_intensity : number
+            The intensity of the sector
+
+        Notes
+        -----
+        Angles are measured in radians. The x > 0 axis is zero degrees. The
+        direction of positive theta is counter-clockwise.
+        """
+        # Get machine parameters
+        source_wavelength = self.source_wavelength
+        sample_to_detector_distance = self.sample_to_detector_distance
+
+        # Check if required machine parameters were provided
+        if not all([self.source_wavelength, self.sample_to_detector_distance]):
+            raise ValueError("You must initialize the units class with machine"
+                    " parameters for this method!")
+
+        # Reference the stored image
+        image = self.image
+
+        # Get the image shape
+        shape = image.shape
+
+        # Initialize the units class
+        units_class = DiffractionUnitsConversion(
+                source_wavelength=source_wavelength, pixel_length=pixel_length,
+                sample_to_detector_distance=sample_to_detector_distance)
+
+        # Set rmin to the larger angstrom spacing (reciprocal space)
+        rmin = units_class.bragg_peak_pixel_location_from_molecular_spacing(
+                amax)
+        # Set rmax to the smaller angstrom spacing (repciprocal space)
+        rmax = units_class.bragg_peak_pixel_location_from_molecular_spacing(
+                amin)
+
+        sector_intensity = self.feature_sector_intensity(
+                center=center, rmin=rmin, rmax=rmin, theta_min=theta_min,
+                theta_max=theta_max)
+
+        return sector_intensity
