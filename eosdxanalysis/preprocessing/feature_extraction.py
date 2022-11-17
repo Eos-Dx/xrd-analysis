e4915e390b5314ff9db68c3aee3ded945b8d8a87
Jonathan Friedman <jonbenfri@gmail.com>
2022-11-17 11:49:01 -0500

    adds sector angle bounds checking and  updates docstring

--- eosdxanalysis/preprocessing/feature_extraction.py
+++ eosdxanalysis/preprocessing/feature_extraction.py
@@ -417,13 +417,10 @@ class FeatureExtraction(object):
         amax : number
             Maximum annulus radius in pixel units
 
-        theta_min : number
-            Start angle of sector in radians. 0 degrees is x > 0 axis. The
-            direction of positive theta is counter-clockwise.
-
-        theta_max : number
-            End angle of sector in radians. 0 degrees is x > 0 axis. The
-            direction of positive theta is counter-clockwise.
+        sector_angle : number
+            Angle of sector in radians. Sector is symmetric about equator.
+            0 degrees is x > 0 axis. The direction of positive theta is
+            counter-clockwise.
 
         Output
         ------
@@ -436,6 +433,16 @@ class FeatureExtraction(object):
         Angles are measured in radians. The x > 0 axis is zero degrees. The
         direction of positive theta is counter-clockwise.
         """
+        if sector_angle < 0 or sector_angle > np.pi:
+            raise ValueError(
+                    "Sector angle bounds must be between 0 and +pi.")
+
+        # Reference the stored image
+        image = self.image
+
+        # Get the image shape
+        shape = image.shape
+
         # Get machine parameters
         source_wavelength = self.source_wavelength
         sample_to_detector_distance = self.sample_to_detector_distance
@@ -444,12 +451,6 @@ class FeatureExtraction(object):
         if not all([self.source_wavelength, self.sample_to_detector_distance]):
             raise ValueError("You must initialize the units class with machine"
                     " parameters for this method!")
-
-        # Reference the stored image
-        image = self.image
-
-        # Get the image shape
-        shape = image.shape
 
         # Initialize the units class
         units_class = DiffractionUnitsConversion(
@@ -463,11 +464,36 @@ class FeatureExtraction(object):
         rmax = units_class.bragg_peak_pixel_location_from_molecular_spacing(
                 amin)
 
-        sector_intensity = self.feature_sector_intensity(
-                center=center, rmin=rmin, rmax=rmin, theta_min=theta_min,
-                theta_max=theta_max)
+        # Create a mask for the annulus
+        annulus_mask = create_circular_mask(shape[0], shape[1], center=center,
+                rmin=rmin, rmax=rmax)
 
-        return sector_intensity
+        # Generate a meshgrid the same size as the image
+        x_end = shape[1]/2 - 0.5
+        x_start = -x_end
+        y_end = x_start
+        y_start = x_end
+        YY, XX = np.mgrid[y_start:y_end:shape[0]*1j, x_start:x_end:shape[1]*1j]
+        TT = np.arctan2(YY, XX)
+
+        # Calculate sector start and end angles based on symmetric sector angle
+        theta_min = -sector_angle/2
+        theta_max = sector_angle/2
+
+        # Get right sector indices
+        right_sector_indices = \
+                (TT > theta_min) & (TT < theta_max) & annulus_mask
+        # Get left sector indices based on left-right symmetry
+        left_sector_indices = np.fliplr(right_sector_indices)
+
+        # Compute right and left sector intensities
+        right_sector_intensity = np.sum(image[right_sector_indices])
+        left_sector_intensity = np.sum(image[left_sector_indices])
+
+        # Compute total equator pair intensity
+        equator_pair_intensity = right_sector_intensity + left_sector_intensity
+
+        return equator_pair_intensity
 
     def feature_sector_intensity_meridian_pair(
             self, center=None, rmin=None, rmax=None,
@@ -487,13 +513,10 @@ class FeatureExtraction(object):
         rmax : number
             Maximum annulus radius in pixel units
 
-        theta_min : number
-            Start angle of sector in radians. 0 degrees is x > 0 axis. The
-            direction of positive theta is counter-clockwise.
-
-        theta_max : number
-            End angle of sector in radians. 0 degrees is x > 0 axis. The
-            direction of positive theta is counter-clockwise.
+        sector_angle : number
+            Angle of sector in radians. Sector is symmetric about equator.
+            0 degrees is x > 0 axis. The direction of positive theta is
+            counter-clockwise.
 
         Output
         ------
@@ -566,13 +589,10 @@ class FeatureExtraction(object):
         amax : number
             Maximum annulus radius in pixel units
 
-        theta_min : number
-            Start angle of sector in radians. 0 degrees is x > 0 axis. The
-            direction of positive theta is counter-clockwise.
-
-        theta_max : number
-            End angle of sector in radians. 0 degrees is x > 0 axis. The
-            direction of positive theta is counter-clockwise.
+        sector_angle : number
+            Angle of sector in radians. Sector is symmetric about equator.
+            0 degrees is x > 0 axis. The direction of positive theta is
+            counter-clockwise.
 
         Output
         ------
@@ -585,38 +605,7 @@ class FeatureExtraction(object):
         Angles are measured in radians. The x > 0 axis is zero degrees. The
         direction of positive theta is counter-clockwise.
         """
-        # Get machine parameters
-        source_wavelength = self.source_wavelength
-        sample_to_detector_distance = self.sample_to_detector_distance
-
-        # Check if required machine parameters were provided
-        if not all([self.source_wavelength, self.sample_to_detector_distance]):
-            raise ValueError("You must initialize the units class with machine"
-                    " parameters for this method!")
-
-        # Reference the stored image
-        image = self.image
-
-        # Get the image shape
-        shape = image.shape
-
-        # Initialize the units class
-        units_class = DiffractionUnitsConversion(
-                source_wavelength=source_wavelength, pixel_length=pixel_length,
-                sample_to_detector_distance=sample_to_detector_distance)
-
-        # Set rmin to the larger angstrom spacing (reciprocal space)
-        rmin = units_class.bragg_peak_pixel_location_from_molecular_spacing(
-                amax)
-        # Set rmax to the smaller angstrom spacing (repciprocal space)
-        rmax = units_class.bragg_peak_pixel_location_from_molecular_spacing(
-                amin)
-
-        sector_intensity = self.feature_sector_intensity(
-                center=center, rmin=rmin, rmax=rmin, theta_min=theta_min,
-                theta_max=theta_max)
-
-        return sector_intensity
+        pass
 
 def feature_extraction(input_path, output_filepath, params):
     """
