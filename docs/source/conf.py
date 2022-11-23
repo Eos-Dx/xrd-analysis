1a63f2d6cac0d3763cdeb4ff0e78c3dbb451de86
Jonathan Friedman <jonbenfri@gmail.com>
2022-11-23 09:27:51 -0500

    updates project version and documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Eos Dx Analysis'
 copyright = '2022, Eos Dx, Inc.'
 author = 'Eos Dx, Inc.'
-release = '2.19.0'
+release = '2.20.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
