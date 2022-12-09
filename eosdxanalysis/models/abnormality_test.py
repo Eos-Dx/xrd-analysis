ac3279d08781af9f8ef3d6b358ea82b0a34b7cd3
Jonathan Friedman <jonbenfri@gmail.com>
2022-12-09 10:43:16 -0500

    adds image brightness batch function for roc analysis

--- eosdxanalysis/models/abnormality_test.py
+++ eosdxanalysis/models/abnormality_test.py
@@ -15,7 +15,7 @@ from eosdxanalysis.preprocessing.image_processing import bright_pixel_count
 
 def image_brightness(
         masked_image, pixel_brightness_threshold=0.75,
-        image_brightness_threshold=0.1, image_area=36220):
+        image_area=36220):
     """
     Predicts abnormality of an image based on brightness.
 
@@ -31,11 +31,6 @@ def image_brightness(
         is mapped to ``0``, and the highest intensity pixel is mapped to ``1``.
         All pixels that are above the relative threshold are considered bright
         pixels.
-
-    image_brightness_threshold : float
-        Relative value to define a bright image. If the ratio of bright pixels
-        to the image area is greater than the relative threshold, the image is
-        considered bright and labeled abnormal.
 
     image_area : int
         The number of pixels in the image to analyze.  Default value is
@@ -53,6 +48,115 @@ def image_brightness(
 
     brightness = bright_pixels/image_area
     return brightness
+
+def image_brightness_batch(
+        patients_db=None, source_data_path=None,
+        pixel_brightness_threshold=None, image_area=36220):
+    """
+    Calculates relative image brightness for a dataset.
+
+    Parameters
+    ----------
+
+    patients_db : str
+        Path to csv file containing two columns, ``Barcode``, and ``Diagnosis``,
+        where ``Barcode`` is a unique patient ID, and ``Diagnosis`` is either
+        ``healthy`` or ``cancer``.
+
+    source_data_path : str
+        Path to preprocessed measurement files. Files should be centered,
+        rotated with beam and outer areas removed.
+
+    pixel_brightness_threshold : float
+        Relative value to define a bright pixel. The lowest intensity pixel is
+        is mapped to ``0``, and the highest intensity pixel is mapped to ``1``.
+        All pixels that are above the relative threshold are considered bright
+        pixels.
+
+    image_area : int
+        The number of pixels in the image to analyze.  Default value is
+        ``36220`` which corresponds to ``rmin=24``, and ``rmax=110``.
+
+    Returns
+    -------
+
+    TP, FP, TN, FN : 4-tuple (int)
+        True positive, false positive, true negative, and false negative
+        counts.
+
+    """
+    # Load patient database file
+    db = pd.read_csv(patients_db, index_col="Barcode")
+
+    # Create a dataframe corresponding to input data filenames
+    filepath_list = glob.glob(os.path.join(source_data_path, "*.txt"))
+    filepath_list.sort()
+
+    df = pd.DataFrame(columns={"Image_Brightness"})
+
+    # Run abnormality test for all data files
+    for filepath in filepath_list[:]:
+        # Extract the filename
+        filename = os.path.basename(filepath)
+        # Load the masked image
+        masked_image = np.loadtxt(filepath)
+        # Run abnormality test
+        brightness = image_brightness(
+                masked_image,
+                pixel_brightness_threshold=pixel_brightness_threshold,
+                image_area=image_area)
+        # Add row to dataframe
+        df.loc[filename] = brightness
+
+    # Add a Barcode column to the dataframe
+    # Extract the first letter and all numbers in the filename before the subindex
+    # E.g., from filename AB12345-01.txt -> A12345 is extracted
+    # Note: Issue if the Barcode format changes
+    extraction = df.index.str.extractall("CR_([A-Z]{1}).*?([0-9]+)")
+    extraction_series = extraction[0] + extraction[1]
+    extraction_list = extraction_series.tolist()
+
+    assert(len(extraction_list) == df.shape[0])
+    df["Barcode"] = extraction_list
+
+    # Merge the databases
+    df_ext = pd.merge(df, db, left_on="Barcode", right_index=True)
+
+
+    ###################
+    # Diagnostic Rule #
+    ###################
+
+    # If any patient has a measurement that is identified as abnormal,
+    # set the Prediction column to 1
+
+    # Get the list of patient Ids
+    patient_id_list = df_ext["Patient_ID"].dropna().unique().tolist()
+
+    # Create an empty patients dataframe
+    df_patients = pd.DataFrame(columns={"Max_Image_Brightness"})
+
+    # Fill the patients dataframe with the maximum image brightness seen
+    for patient_id in patient_id_list:
+        patient_slice = df_ext[df_ext["Patient_ID"] == patient_id]
+        max_image_brightness = patient_slice["Image_Brightness"].max()
+        df_patients.loc[patient_id] = max_image_brightness
+
+    # Get patients and associated diagnosis
+    db_patients = db[["Patient_ID", "Diagnosis"]].drop_duplicates()
+    # Merge to get patient diagnosis and prediction in the same dataframe
+    df_patients_ext = pd.merge(
+            df_patients, db_patients, left_index=True, right_on="Patient_ID")
+    # Set patient id as index
+    df_patients_ext.index = df_patients_ext["Patient_ID"]
+    # Extract diagnosis and prediction
+    df_patients_ext = df_patients_ext[["Diagnosis", "Max_Image_Brightness"]]
+
+    # Prepare outputs for ROC analysis
+    y_true = df_patients_ext["Diagnosis"].replace("cancer",1).replace("healthy",0)
+    y_score = df_patients_ext["Max_Image_Brightness"]
+
+    return y_true, y_score
 
 def abnormality_test(
         masked_image, pixel_brightness_threshold=0.75,
@@ -92,7 +196,6 @@ def abnormality_test(
     brightness = image_brightness(
                 masked_image,
                 pixel_brightness_threshold=pixel_brightness_threshold,
-                image_brightness_threshold=image_brightness_threshold,
                 image_area=image_area)
 
     abnormal = brightness > image_brightness_threshold
@@ -116,6 +219,9 @@ def abnormality_test_batch(
     source_data_path : str
         Path to preprocessed measurement files. Files should be centered,
         rotated with beam and outer areas removed.
+
+    output_filepath : str
+        Path to store patient predictions csv file.
 
     pixel_brightness_threshold : float
         Relative value to define a bright pixel. The lowest intensity pixel is
