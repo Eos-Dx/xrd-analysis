7085d98a59490f29c155fab3cd934bb6f950121a
Jonathan Friedman <jonbenfri@gmail.com>
2022-12-21 13:49:06 -0500

    adds inner annulus feature

--- examples/pca_plot.py
+++ examples/pca_plot.py
@@ -32,8 +32,9 @@ feature_list = [
         # 'total_flux',
         # 'bright_pixel_count',
         # 'max_intensity',
-        'annulus_intensity_9A',
-        'annulus_intensity_5A',
+        # 'annulus_intensity_9A',
+        # 'annulus_intensity_5A',
+        'annulus_intensity_inner',
         'annulus_intensity_4A',
         'sector_intensity_equator_pair_9A',
         'sector_intensity_meridian_pair_9A',
@@ -86,12 +87,17 @@ for idx in range(n_components):
 
 # Transform data using PCA
 X_pca = estimator.transform(df.values)
+X_train_pca = estimator.transform(df_train.values)
+
+# Set offsets
+x_label_offset = 0.01
+y_label_offset = 0.01
 
 ###################################
 # 3D PCA subspace projection plot #
 ###################################
 
-if True:
+if False:
 
     # Show 3D surface maps
     plot_title = "3D {}".format(title)
@@ -115,36 +121,35 @@ if True:
 # PCA subspace projection plot #
 ################################
 
-# Plot PCA-reduced dataset with file labels
-plot_title = title
-fig, ax = plt.subplots(figsize=aspect, num=plot_title, tight_layout=True)
-fig.suptitle(title)
-plt.scatter(X_pca[:,0], X_pca[:,1])
+if False:
 
-# Set offsets
-x_label_offset = 0.01
-y_label_offset = 0.01
+    # Plot PCA-reduced dataset with file labels
+    plot_title = title
+    fig, ax = plt.subplots(figsize=aspect, num=plot_title, tight_layout=True)
+    fig.suptitle(title)
+    plt.scatter(X_pca[:,0], X_pca[:,1])
 
-filename_list = df.index.to_list()
 
-# Annotate data points with filenames
-for i, filename in enumerate(filename_list):
-    ax.annotate(
-        filename.replace("CR_","").replace(".txt",""),
-        (X_pca[i,0], X_pca[i,1]),
-        xytext=(X_pca[i,0]+x_label_offset, X_pca[i,1]+y_label_offset))
+    filename_list = df.index.to_list()
 
-# Label plot axes and title
-plt.xlabel("PC0")
-plt.ylabel("PC1")
+    # Annotate data points with filenames
+    for i, filename in enumerate(filename_list):
+        ax.annotate(
+            filename.replace("CR_","").replace(".txt",""),
+            (X_pca[i,0], X_pca[i,1]),
+            xytext=(X_pca[i,0]+x_label_offset, X_pca[i,1]+y_label_offset))
 
-plt.show()
+    # Label plot axes and title
+    plt.xlabel("PC0")
+    plt.ylabel("PC1")
+
+    plt.show()
 
 #################
 #  Subsets plot
 #################
 
-if True:
+if False:
 
     # Collect data subsets for plotting
     series_dict = {
@@ -181,7 +186,7 @@ if True:
 #  3D Subsets plot
 ###################
 
-if True:
+if False:
 
     # Collect data subsets for plotting
     series_dict = {
@@ -331,18 +336,20 @@ if True:
         ax.scatter(
                 X_plot[:,0], X_plot[:,1], X_plot[:,2], label=diagnosis, c=colors[diagnosis])
 
-
     if False:
         # Annotate data points with filenames
         for i, filename in enumerate(df_ext.index):
-            ax.annotate(
-                filename.replace("CR_","").replace(".txt",""),
-                (X_pca[i,0], X_pca[i,1]),
-                xytext=(X_pca[i,0]+x_label_offset, X_pca[i,1]+y_label_offset))
+            label = filename.replace("CR_","").replace(".txt","")
+            ax.text(X_pca[i,0], X_pca[i,1], X_pca[i,2], label)
 
     ax.set_xlabel("PC0")
     ax.set_ylabel("PC1")
     ax.set_zlabel("PC2")
+
+    # Set axis limits
+    ax.set_xlim([-5, 2])
+    ax.set_ylim([-2, 2])
+    ax.set_zlim([-0.5, 1.5])
 
     fig.tight_layout()
 
@@ -354,81 +361,133 @@ if True:
 # K-means plots #
 #################
 
-if False:
+if True:
 
     # df_pca = data + kmeans cluster labels
-    df_pca = pd.DataFrame(data=X_pca, index=df.index)
+    df_pca = pd.DataFrame(data=X_train_pca, index=df_train.index)
     # df_pca.to_csv(kmeans_filepath)
 
-    cluster_count_min = 2
-    cluster_count_max = 6
+    cluster_count_min = 20
+    cluster_count_max = 20
 
     # Run K-means on pca-reduced features
     for idx in range(cluster_count_min, cluster_count_max+1):
 
         cluster_count = idx
 
-        title = "K-Means on PCA-reduced Xena Dataset with {} clusters".format(idx)
-        fig, ax = plt.subplots(figsize=aspect, num=title, tight_layout=True)
-        fig.suptitle(title)
+        if True:
+            title = "K-Means on PCA-reduced Xena Dataset with {} clusters".format(idx)
+            fig, ax = plt.subplots(figsize=aspect, num=title, tight_layout=True)
+            fig.suptitle(title)
 
         # Run k-means
-        reduced_data = X_pca[:,:2]
+        reduced_data = X_train_pca
         kmeans = KMeans(cluster_count)
         kmeans.fit(reduced_data)
 
         df_pca["kmeans_{}".format(idx)] = kmeans.labels_
 
-        # Plot decisision boundaries
-        # Step size of the mesh. Decrease to increase the quality of the VQ.
-        h = 0.01  # point in the mesh [x_min, x_max]x[y_min, y_max].
+        if True:
 
-        # Plot the decision boundary. For that, we will assign a color to each
-        x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
-        y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+            # Plot decision boundaries
+            # Step size of the mesh. Decrease to increase the quality of the VQ.
+            h = 0.01  # point in the mesh [x_min, x_max]x[y_min, y_max].
 
-        # Obtain labels for each point in mesh. Use last trained model.
-        Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
+            # Plot the decision boundary. For that, we will assign a color to each
+            x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+            y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+            xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
-        plt.imshow(
-            Z,
-            interpolation="nearest",
-            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-            cmap=plt.cm.Paired,
-            aspect="auto",
-            origin="lower",
-        )
+            # Obtain labels for each point in mesh. Use last trained model.
+            Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+            # Put the result into a color plot
+            Z = Z.reshape(xx.shape)
 
-        # Plot the data
-        plt.plot(reduced_data[:, 0], reduced_data[:, 1], "k.", markersize=10)
+            plt.imshow(
+                Z,
+                interpolation="nearest",
+                extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+                cmap=plt.cm.Paired,
+                aspect="auto",
+                origin="lower",
+            )
 
-        # Plot the centroids
-        centroids = kmeans.cluster_centers_
-        print("Centroids:")
-        print(centroids)
-        plt.scatter(
-                centroids[:, 0],
-                centroids[:, 1],
-                marker="x",
-                s=200,
-                linewidths=3,
-                color="w",
-                zorder=10000,
-                )
+            # Plot the data
+            plt.plot(reduced_data[:, 0], reduced_data[:, 1], "k.", markersize=10)
 
-        plt.xlabel("PC0")
-        plt.ylabel("PC1")
+            # Plot the centroids
+            centroids = kmeans.cluster_centers_
+            print("Centroids:")
+            print(centroids)
+            plt.scatter(
+                    centroids[:, 0],
+                    centroids[:, 1],
+                    marker="x",
+                    s=200,
+                    linewidths=3,
+                    color="w",
+                    zorder=10000,
+                    )
 
-    # Show K-means plots
-    plt.show()
+            plt.xlabel("PC0")
+            plt.ylabel("PC1")
+
+        # Show K-means plots
+        plt.show()
+
+
+    extraction = df_pca.index.str.extractall("CR_([A-Z]{1}).*?([0-9]+)")
+    extraction_series = extraction[0] + extraction[1].str.zfill(5)
+    extraction_list = extraction_series.tolist()
+
+    assert(len(extraction_list) == df_pca.shape[0])
 
     df_pca_ext = df_pca.copy()
     df_pca_ext["Barcode"] = extraction_list
     df_pca_ext = pd.merge(df_pca_ext, db, left_on="Barcode", right_index=True)
-    df_pca_ext = df_pca_ext.rename(columns={0: "PC0", 1: "PC1"})
+    df_pca_ext = df_pca_ext.rename(columns={0: "PC0", 1: "PC1", 2: "PC2"})
 
     # Save dataframe
     df_pca_ext.to_csv(kmeans_filepath)
+
+#if __name__ == '__main__':
+#    """
+#    # Run PCA and generate plots
+#    """
+#    # Set up argument parser
+#    parser = argparse.ArgumentParser()
+#    # Set up parser arguments
+#    parser.add_argument(
+#            "--input_filepath", default=None, required=True,
+#            help="The csv input file containing data to perform quality"
+#                " control on.")
+#    parser.add_argument(
+#            "--output_filepath", default=None, required=True,
+#            help="The csv output file path to store the quality control data.")
+#    parser.add_argument(
+#            "--patients_filepath", default=None, required=False,
+#            help="The csv input file containing data to perform quality"
+#                " control on.")
+#
+#    args = parser.parse_args()
+#
+#    csv_input_filepath = args.csv_input_filepath
+#    csv_output_filepath = args.csv_output_filepath
+#
+#    no_add_columns = args.no_add_columns
+#    no_qc_pass_fail_output_folders = args.no_qc_pass_fail_output_folders
+#    preprocessed_data_path = args.preprocessed_data_path
+#
+#
+#    # Get exclusion criteria from file
+#    criteria_file = args.criteria_file
+#    if criteria_file:
+#        with open(criteria_file,"r") as criteria_fp:
+#            criteria = json.loads(criteria_fp.read())
+#    else:
+#        raise ValueError("Control criteria file required.")
+#
+#    main(
+#            csv_input_filepath, csv_output_filepath, criteria, no_add_columns,
+#            no_qc_pass_fail_output_folders, preprocessed_data_path)
+#
