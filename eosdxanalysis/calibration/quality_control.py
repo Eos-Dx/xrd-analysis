dda063ea77f4084ec54ccf06222f60c5eaa91c7c
benblinchevsky <bblinchevsky@geosdx.com>
2023-02-15 17:13:46 +0100

    adds file about text

---
+++ eosdxanalysis/calibration/quality_control.py
@@ -0,0 +1,4 @@
+# The file is to provide automated quality control tool for technicians to check the initial beam quality
+# It checks the following:
+#   1. The beam aperture alignment
+#   2. The beam detector alignment
