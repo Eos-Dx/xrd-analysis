0fc34e19b6b30a05795dd083819b232337eb6a7d
Jonathan Friedman <jonbenfri@gmail.com>
2023-02-16 18:32:43 -0500

    removes hardcoded young dogs list

--- examples/svm.py
+++ examples/svm.py
@@ -30,157 +30,17 @@ from eosdxanalysis.models.utils import scale_features
 from eosdxanalysis.models.utils import add_patient_data
 
 
-def run_svm(df_train_path, db_path, output_path=None, scale_by=None):
+def run_svm(df_train_path, db_path=None, output_path=None, scale_by=None):
     # Generate timestamp
     timestr = "%Y%m%dT%H%M%S.%f"
     timestamp = datetime.utcnow().strftime(timestr)
 
     df_train = pd.read_csv(df_train_path, index_col="Filename")
-    # Add patient data
-    df_train = add_patient_data(df_train, db_path, index_col="Barcode").dropna().copy()
+    if db_path:
+        # Add patient data
+        df_train = add_patient_data(df_train, db_path, index_col="Barcode").dropna().copy()
+
     feature_list = np.arange(1,51).astype(str)
-    df_train["total_intensity"] = np.sum(df_train[feature_list].values, axis=1).astype(int)
-
-    young_dogs_list = [
-        "CR_A00120-10.txt",
-        "CR_A00120-1.txt",
-        "CR_A00120-2.txt",
-        "CR_A00120-3.txt",
-        "CR_A00120-4.txt",
-        "CR_A00120-5.txt",
-        "CR_A00120-6.txt",
-        "CR_A00120-7.txt",
-        "CR_A00120-8.txt",
-        "CR_A00120-9.txt",
-        "CR_A00131-10.txt",
-        "CR_A00131-1.txt",
-        "CR_A00131-2.txt",
-        "CR_A00131-3.txt",
-        "CR_A00131-4.txt",
-        "CR_A00131-5.txt",
-        "CR_A00131-6.txt",
-        "CR_A00131-7.txt",
-        "CR_A00131-8.txt",
-        "CR_A00131-9.txt",
-        "CR_A00134-1.txt",
-        "CR_A00134-2.txt",
-        "CR_A00134-3.txt",
-        "CR_A00134-4.txt",
-        "CR_A00134-5.txt",
-        "CR_A00134-6.txt",
-        "CR_A00134-7.txt",
-        "CR_A00150-1.txt",
-        "CR_A00150-2.txt",
-        "CR_A00150-3.txt",
-        "CR_A00150-4.txt",
-        "CR_A00150-5.txt",
-        "CR_A00150-6.txt",
-        "CR_A00150-7.txt",
-        "CR_A00150-8.txt",
-        "CR_A00150-9.txt",
-        "CR_A00153-10.txt",
-        "CR_A00153-1.txt",
-        "CR_A00153-2.txt",
-        "CR_A00153-3.txt",
-        "CR_A00153-4.txt",
-        "CR_A00153-5.txt",
-        "CR_A00153-6.txt",
-        "CR_A00153-7.txt",
-        "CR_A00153-8.txt",
-        "CR_A00153-9.txt",
-        "CR_A00156-1.txt",
-        "CR_A00156-2.txt",
-        "CR_A00156-3.txt",
-        "CR_A00156-4.txt",
-        "CR_A00156-5.txt",
-        "CR_A00156-6.txt",
-        "CR_A00156-7.txt",
-        "CR_A00159-1.txt",
-        "CR_A00159-2.txt",
-        "CR_A00159-3.txt",
-        "CR_A00159-4.txt",
-        "CR_A00159-5.txt",
-        "CR_A00159-6.txt",
-        "CR_A00159-7.txt",
-        "CR_A00171-1.txt",
-        "CR_A00171-2.txt",
-        "CR_A00171-3.txt",
-        "CR_A00171-4.txt",
-        "CR_A00171-5.txt",
-        "CR_A00171-6.txt",
-        "CR_A00171-7.txt",
-        "CR_A00175-1.txt",
-        "CR_A00175-2.txt",
-        "CR_A00175-3.txt",
-        "CR_A00175-4.txt",
-        "CR_A00175-5.txt",
-        "CR_A00178-1.txt",
-        "CR_A00178-2.txt",
-        "CR_A00178-3.txt",
-        "CR_A00178-4.txt",
-        "CR_A00178-5.txt",
-        "CR_A00178-6.txt",
-        "CR_A00178-7.txt",
-        "CR_A00184-10.txt",
-        "CR_A00184-1.txt",
-        "CR_A00184-2.txt",
-        "CR_A00184-3.txt",
-        "CR_A00184-4.txt",
-        "CR_A00184-5.txt",
-        "CR_A00184-6.txt",
-        "CR_A00184-7.txt",
-        "CR_A00184-8.txt",
-        "CR_A00184-9.txt",
-        "CR_A00190-1.txt",
-        "CR_A00190-2.txt",
-        "CR_A00190-3.txt",
-        "CR_A00190-4.txt",
-        "CR_A00190-5.txt",
-        "CR_A00190-6.txt",
-        "CR_A00190-7.txt",
-        "CR_A00193-1.txt",
-        "CR_A00193-2.txt",
-        "CR_A00193-3.txt",
-        "CR_A00199-1.txt",
-        "CR_A00199-2.txt",
-        "CR_A00199-3.txt",
-        "CR_A00199-4.txt",
-        "CR_A00199-5.txt",
-        "CR_A00199-6.txt",
-        "CR_A00202-1.txt",
-        "CR_A00202-2.txt",
-        "CR_A00202-3.txt",
-        "CR_A00202-4.txt",
-        "CR_A00202-5.txt",
-        "CR_A00202-6.txt",
-        "CR_A00202-7.txt",
-        "CR_A00250-1.txt",
-        "CR_A00250-2.txt",
-        "CR_A00250-3.txt",
-        "CR_A00250-4.txt",
-        "CR_A00262-1.txt",
-        "CR_A00262-2.txt",
-        "CR_A00262-3.txt",
-        "CR_A00262-4.txt",
-        "CR_A00266-10.txt",
-        "CR_A00266-1.txt",
-        "CR_A00266-2.txt",
-        "CR_A00266-3.txt",
-        "CR_A00266-4.txt",
-        "CR_A00266-5.txt",
-        "CR_A00266-6.txt",
-        "CR_A00266-7.txt",
-        "CR_A00266-8.txt",
-        "CR_A00266-9.txt",
-    ]
-
-
-    df_young = df_train[df_train.index.isin(young_dogs_list)].copy()
-    df_old = df_train[~df_train.index.isin(young_dogs_list)].copy()
-
-    df_train.loc[df_young.index, "y_true"] = 0
-    df_train.loc[df_old.index, "y_true"] = 1
-    df_train["y_true"] = df_train["y_true"].astype(int)
 
     # Get training data
     if scale_by:
@@ -190,10 +50,12 @@ def run_svm(df_train_path, db_path, output_path=None, scale_by=None):
         X = df_train[feature_list].values
 
     # Get training labels
+    df_train["y_true"] = (df_train["Diagnosis"] == "cancer").astype(int)
     y = df_train["y_true"].values
 
     # Create classifier
-    clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+    svm = SVC(C=1, kernel='rbf', gamma='auto', verbose=True)
+    clf = make_pipeline(StandardScaler(), svm)
 
     # Train model
     clf.fit(X, y)
@@ -274,7 +136,7 @@ if __name__ == '__main__':
             "--df_train_path", type=str, default=None, required=True,
             help="The path to training features")
     parser.add_argument(
-            "--db_path", type=str, default=None, required=True,
+            "--db_path", type=str, default=None, required=False,
             help="The path to patient database")
     parser.add_argument(
             "--output_path", type=str, default=None, required=False,
