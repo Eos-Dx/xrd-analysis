e3521cc3009d35942578a0a3ca398d8353f394a3
Jonathan Friedman <jonbenfri@gmail.com>
2023-04-24 11:05:20 -0400

    updates output files locations

--- eosdxanalysis/preprocessing/azimuthal_preprocess.py
+++ eosdxanalysis/preprocessing/azimuthal_preprocess.py
@@ -28,10 +28,10 @@ from eosdxanalysis.calibration.utils import radial_profile_unit_conversion
 
 
 def run_azimuthal_preprocessing(
-        input_path, output_path, input_path_list=None,
-        sample_distance_filepath=None,
+        input_path, output_path=None,
         input_dataframe_filepath=None,
-        find_sample_distance_filepath=False,
+        sample_distance_filepath=None,
+        find_sample_distance_filepath=None,
         beam_rmax=15, visualize=False):
     """
     """
@@ -44,8 +44,11 @@ def run_azimuthal_preprocessing(
         # Sort files list
         filepath_list.sort()
     elif input_dataframe_filepath:
-        input_df = pd.read_csv(input_dataframe_filepath, index_col="Filepath")
-        filepath_list = input_df.index.tolist()
+        input_path = os.path.basename(input_dataframe_filepath)
+        input_df = pd.read_csv(input_dataframe_filepath, index_col="Filename")
+        filepath_list = input_df["Filepath"].tolist()
+
+    parent_path = os.path.dirname(input_path)
 
     # Set timestamp
     timestr = "%Y%m%dT%H%M%S.%f"
@@ -55,31 +58,14 @@ def run_azimuthal_preprocessing(
     # Create output directory if it does not exist
     if not output_path:
         # Create preprocessing results directory
-        parent_path = os.path.dirname(input_path)
         results_dir = "preprocessed_results"
         results_path = os.path.join(parent_path, results_dir)
 
         # Create timestamped output directory
         input_dir = os.path.basename(input_path)
-        output_dir = "preprocessed_{}_{}".format(
-                input_dir,
+        output_dir = "preprocessed_results_{}".format(
                 timestamp)
         output_path = os.path.join(results_path, output_dir)
-
-    if find_sample_distance_filepath:
-        # Given a single sample distance file path
-        # Try to locate the sample distance filepath:
-        parent_path = os.path.dirname(input_path)
-        calibration_results_dir = "calibration_results"
-        sample_distance_path = os.path.join(
-                parent_path, calibration_results_dir)
-
-        # Find files
-        calibration_results_filepath_list = glob.glob(os.path.join(sample_distance_path, "*.json"))
-        if len(calibration_results_filepath_list) != 1:
-            raise ValueError("Only a single calibration file is allowed.")
-        else:
-            sample_distance_filepath = calibration_results_filepath_list[0]
 
     # Data output path
     data_output_dir = "data"
@@ -90,6 +76,9 @@ def run_azimuthal_preprocessing(
     image_output_dir = "images"
     image_output_path = os.path.join(output_path, image_output_dir)
     os.makedirs(image_output_path, exist_ok=True)
+
+    print("Saving data to\n{}".format(data_output_path))
+    print("Saving images to\n{}".format(image_output_path))
 
     # Loop over files list
     for filepath in filepath_list:
@@ -132,6 +121,17 @@ def run_azimuthal_preprocessing(
         data_output_filepath = os.path.join(data_output_path,
                 data_output_filename)
 
+        if find_sample_distance_filepath:
+            calibration_results_dir = "calibration_results"
+            sample_distance_path = os.path.join(
+                    parent_path, calibration_results_dir)
+            try:
+                sample_distance_filepath = glob.glob(os.path.join(
+                    sample_distance_path, "*.json"))[0]
+            except IndexError as err:
+                print("No sample distance file found.")
+                raise err
+
         if sample_distance_filepath:
             with open(sample_distance_filepath, "r") as distance_fp:
                 sample_distance_results = json.loads(distance_fp.read())
@@ -145,7 +145,7 @@ def run_azimuthal_preprocessing(
             results = np.hstack([q_range.reshape(-1,1), radial_profile.reshape(-1,1)])
             np.savetxt(data_output_filepath, results)
         elif input_dataframe_filepath:
-            sample_distance_m = input_df.loc[filepath, "Sample_Distance_m"]
+            sample_distance_m = input_df.loc[filename, "Sample_Distance_m"]
 
             # Combine radial profile intensity and q-units
             q_range = radial_profile_unit_conversion(
@@ -154,11 +154,16 @@ def run_azimuthal_preprocessing(
                     radial_units="q_per_nm")
             results = np.hstack([q_range.reshape(-1,1), radial_profile.reshape(-1,1)])
             np.savetxt(data_output_filepath, results)
+
+            # Save q-min and q-max to dataframe
+            input_df.loc[filename, "q_min"] = q_range.min()
+            input_df.loc[filename, "q_max"] = q_range.max()
+
         else:
             np.savetxt(data_output_filepath, radial_profile)
 
         # Save image preview to file
-        plot_title = "Azimuthal Integration {}".format(filename)
+        plot_title = "Radial Intensity Profile {}".format(filename)
         fig = plt.figure(plot_title)
 
         if sample_distance_m:
@@ -184,6 +189,13 @@ def run_azimuthal_preprocessing(
 
         plt.close(fig)
 
+    # Save dataframe ext
+    if input_dataframe_filepath:
+        input_dataframe_filename = os.path.basename(input_dataframe_filepath)
+        output_dataframe_filename = input_dataframe_filename.replace(".", "_ext.")
+        output_dataframe_filepath = os.path.join(
+                output_path, output_dataframe_filename)
+        input_df.to_csv(output_dataframe_filepath, index=True)
 
 if __name__ == '__main__':
     """
@@ -194,9 +206,6 @@ if __name__ == '__main__':
     # Set up parser arguments
     parser.add_argument(
             "--input_path", type=str, required=False,
-            help="The path to data to extract features from")
-    parser.add_argument(
-            "--input_path_list", type=str, required=False,
             help="The path to data to extract features from")
     parser.add_argument(
             "--input_dataframe_filepath", type=str, required=False,
@@ -221,23 +230,20 @@ if __name__ == '__main__':
 
     input_path = os.path.abspath(args.input_path) if args.input_path \
             else None
-    if not input_path:
-        input_path_list = args.input_path_list
     output_path = os.path.abspath(args.output_path) if args.output_path \
             else None
     input_dataframe_filepath = os.path.abspath(args.input_dataframe_filepath) \
             if args.input_dataframe_filepath else None
-    find_sample_distance_filepath = args.find_sample_distance_filepath
     beam_rmax = args.beam_rmax
+    find_sample_distance_filepath = args.find_sample_distance_filepath 
     sample_distance_filepath = args.sample_distance_filepath
     visualize = args.visualize
 
     run_azimuthal_preprocessing(
         input_path=input_path,
         output_path=output_path,
-        input_path_list=input_path_list,
         input_dataframe_filepath=input_dataframe_filepath,
-        beam_rmax=beam_rmax,
         find_sample_distance_filepath=find_sample_distance_filepath,
+        beam_rmax=beam_rmax,
         visualize=visualize,
         )
