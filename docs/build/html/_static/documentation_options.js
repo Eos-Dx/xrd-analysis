904eb88bbb98c6e107e51c8c3d0b7d0cd73a1651
Jonathan Friedman <jonbenfri@gmail.com>
2023-11-20 14:59:11 -0500

    updates project version and documentation

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.76.2',
+    VERSION: '2.77.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
