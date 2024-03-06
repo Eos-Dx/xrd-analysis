8de649462278fbaff2af05678287b4bdec3f30f6
Oleksii <lesha.avdeev@gmail.com>
2024-03-07 00:41:57 +0200

    Change setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+"""Setup file for pip package"""
+
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="xrd-analysis",
     packages=find_packages(),
     version="0.1.0",
     description="Repository for collaboration of EOSDX team",
