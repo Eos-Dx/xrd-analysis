9a798600b53a9b36839f38146518876b28db85a2
Oleksii <lesha.avdeev@gmail.com>
2024-03-08 00:18:35 +0200

    Refactor package setup

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="xrd-analysis",
-    packages=find_packages(),
+    name="xrdanalysis",
     version="0.1.0",
     description="Repository for collaboration of EOSDX team",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     author="EOSDX",
     license="",
 )
