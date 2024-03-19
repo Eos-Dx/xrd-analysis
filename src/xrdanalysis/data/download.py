7b7f3a87408f8755bfaf385bd7e2ccad3cde47bb
Oleksii <lesha.avdeev@gmail.com>
2024-03-19 18:20:41 +0200

    Refactor docstrings

--- src/xrdanalysis/data/download.py
+++ src/xrdanalysis/data/download.py
@@ -20,7 +20,15 @@ URL = "https://api.eosdx.com/api/getmultiple"
 class RequestDB:
     """
     RequestDB is a dataclass used to store essential info for
-    requests to the server
+    requests to the server.
+
+    Attributes:
+        api_key (str): The API key used for authentication.
+        form (Dict[str, str]): A dictionary containing form data for the
+        request.
+        url (str): The URL of the server. Defaults to a predefined URL.
+        unzip_path (Union[str, Path]): The path where downloaded files should
+        be unzipped. Defaults to a predefined path.
     """
 
     api_key: str
@@ -31,13 +39,19 @@ class RequestDB:
 
 def download_data(api_key: str, form={}, url=URL, where="data.zip"):
     """
-    The function download the required data from eosdx DB
-    :param api_key: the API key as string
-    :param form: JSON like dict request form, see API.md
-    description for information
-    form = {'key': 'your-access-key', 'cancer_tissue': True,
-     'measurement_id': '< 3', 'measurement_date': '2023-04-07'}
-    :param url: sharepoint
+    Download the required data from the EOSDX DB.
+
+    :param api_key: The API key as a string.
+    :param form: JSON-like dictionary request form. See API.md for more
+    information.
+                 Example form: {'key': 'your-access-key',
+                                'cancer_tissue': True,
+                                'measurement_id': '< 3',
+                                'measurement_date': '2023-04-07'}
+                 Defaults to an empty dictionary.
+    :param url: The URL of the server. Defaults to a predefined URL.
+    :param where: The location where the downloaded data will be saved.
+    Defaults to "data.zip".
     :return: None
     """
     form["key"] = api_key
@@ -50,7 +64,11 @@ def download_data(api_key: str, form={}, url=URL, where="data.zip"):
 
 def unzip_data(unzip_path=UNZIP_PATH):
     """
-    unzip data downloaded from the server to unzip_path folder
+    Unzip data downloaded from the server to the specified unzip_path folder.
+
+    :param unzip_path: The path where the data will be extracted.
+                       Defaults to UNZIP_PATH if not provided.
+    :return: None
     """
     with zipfile.ZipFile("data.zip", "r") as zf:
         zf.extractall(unzip_path)
@@ -58,8 +76,12 @@ def unzip_data(unzip_path=UNZIP_PATH):
 
 def form_df(unzip_path=UNZIP_PATH) -> pd.DataFrame:
     """
-    generates pandas dataframe according to the data downloaded from the DB
-    using eosdx API
+    Generates a pandas DataFrame according to the data downloaded from the DB
+    using the EOSDX API.
+
+    :param unzip_path: The path where the downloaded data is extracted.
+    Defaults to UNZIP_PATH if not provided.
+    :return: A pandas DataFrame containing the data.
     """
     df = pd.read_csv(unzip_path / "description.csv")
     # MANDATORY!!!
@@ -78,8 +100,13 @@ def form_df(unzip_path=UNZIP_PATH) -> pd.DataFrame:
 
 def get_df(request: RequestDB) -> pd.DataFrame:
     """
-    This is function that makes a requst to the DB and returns
-    pandas dataframe
+    Makes a request to the database using the provided RequestDB object
+    and returns a pandas DataFrame.
+
+    :param request: A RequestDB object containing essential information
+    for the request.
+    :return: A pandas DataFrame containing the data retrieved from
+    the database.
     """
     download_data(api_key=request.api_key, form=request.form, url=request.url)
     unzip_data(request.unzip_path)
