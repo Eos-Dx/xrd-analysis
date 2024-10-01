4d030cb0468fcb90bbb1efe20b5d7a9eeb335bb7
confucii <lesha.avdeev@gmail.com>
2024-10-01 11:12:02 -0400

    Add transformers for pipeline and SD predictors

--- src/xrdanalysis/data_processing/transformers.py
+++ src/xrdanalysis/data_processing/transformers.py
@@ -10,11 +10,17 @@ import numpy as np
 import pandas as pd
 from sklearn.base import TransformerMixin
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import Normalizer, StandardScaler
 
 from xrdanalysis.data_processing.azimuthal_integration import (
     perform_azimuthal_integration,
 )
-from xrdanalysis.data_processing.containers import MLClusterContainer
+from xrdanalysis.data_processing.containers import (
+    MLClusterContainer,
+    ModelScale,
+    Limits,
+    Rule,
+)
 from xrdanalysis.data_processing.utility_functions import (
     create_mask,
     generate_poni,
@@ -157,13 +163,159 @@ COLUMNS_DEF = [
 ]
 
 
+class ColumnNormalizer(TransformerMixin):
+    """
+    Transformer class for normalizing arrays in a specific column of a
+    DataFrame to be used in an sklearn pipeline.
+
+    :param column: The name of the column containing arrays to be normalized.
+    :type column: str
+    :param norm: The type of norm to use for normalization \
+    ('l1', 'l2', or 'max'). Defaults to 'l1'.
+    :type norm: str
+    """
+
+    def __init__(self, column, norm="l1"):
+        """
+        Initializes the ColumnNormalizer with the specified column name and
+        normalization method.
+
+        :param column: The name of the column containing arrays to normalize.
+        :type column: str
+        :param norm: The type of norm to use for normalization. Can be 'l1', \
+        'l2', or 'max'. Defaults to 'l2'.
+        :type norm: str
+        """
+        self.column = column
+        self.normalizer = Normalizer(norm=norm)
+
+    def fit(self, X, y=None):
+        """
+        No fitting required for the Normalizer (stateless), but this method
+        is required for compatibility with sklearn pipelines.
+
+        :param X: Input DataFrame.
+        :type X: pd.DataFrame
+        :param y: Ignored, exists for compatibility with sklearn pipeline.
+        :type y: None
+        :return: The fitted transformer.
+        :rtype: ColumnNormalizer
+        """
+        return self
+
+    def transform(self, X, y=None):
+        """
+        Transforms the specified column by normalizing the arrays in each row.
+
+        :param X: Input DataFrame with a column containing arrays to normalize.
+        :type X: pd.DataFrame
+        :param y: Ignored, exists for compatibility with sklearn pipeline.
+        :type y: None
+        :return: DataFrame with the specified column normalized.
+        :rtype: pd.DataFrame
+        """
+        X_copy = X.copy()
+        X_copy[self.column] = X_copy[self.column].apply(
+            lambda arr: self.normalizer.transform([arr])[0]
+        )
+        return X_copy
+
+
+class ColumnExtractor(TransformerMixin):
+    """
+    Transformer class for flattening arrays and appending values from
+    specified columns in a DataFrame to be used in an sklearn pipeline.
+
+    :param columns: List of column names to flatten and combine.
+    :type columns: List[str]
+    """
+
+    def __init__(self, columns):
+        """
+        Initializes the ColumnFlattener with the specified columns.
+
+        :param columns: List of column names to flatten and combine.
+        :type columns: List[str]
+        """
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        """
+        Fit method is not required for ColumnFlattener, but it is
+        provided for compatibility with sklearn pipelines.
+
+        :param X: Input DataFrame.
+        :type X: pd.DataFrame
+        :param y: Ignored, exists for compatibility with sklearn pipeline.
+        :type y: None
+        :return: The fitted transformer.
+        :rtype: ColumnFlattener
+        """
+        return self
+
+    def transform(self, X, y=None):
+        """
+        Transforms the specified columns by flattening any arrays and
+        appending the values from each column into a single list for
+        each row.
+
+        :param X: Input DataFrame with columns to flatten.
+        :type X: pd.DataFrame
+        :param y: Ignored, exists for compatibility with sklearn pipeline.
+        :type y: None
+        :return: DataFrame where each row is a flattened list of values from \
+        the specified columns.
+        :rtype: pd.DataFrame
+        """
+        X_copy = X.copy()
+
+        # Apply flattening logic to each row
+        flattened_data = X_copy.apply(
+            lambda row: self._flatten_row(row), axis=1
+        )
+
+        # Return the DataFrame with flattened rows
+        return pd.DataFrame(
+            np.asarray(flattened_data.values.tolist()),
+            index=flattened_data.index,
+        )
+
+    def _flatten_row(self, row):
+        """
+        Helper function that flattens the values of the specified columns
+        in a row.
+
+        :param row: A single row of the DataFrame.
+        :type row: pd.Series
+        :return: A flattened list of values from the specified columns.
+        :rtype: List
+        """
+        flattened_list = []
+        for col in self.columns:
+            value = row[col]
+            if isinstance(value, (list, np.ndarray)):
+                # Flatten arrays or lists
+                flattened_list.extend(value)
+            else:
+                # Append single values
+                flattened_list.append(value)
+        return flattened_list
+
+
 class DataPreparation(TransformerMixin):
     """
     Transformer class to prepare a raw DataFrame according to the standard.
     """
 
-    def __init__(self, columns=COLUMNS_DEF):
+    def __init__(
+        self,
+        columns=COLUMNS_DEF,
+        limits: Limits = None,
+        cleaning_rules: List[Rule] = None,
+    ):
         self.columns = columns
+        self.limits = limits
+        self.cleaning_rules = cleaning_rules
 
     def fit(self, x: pd.DataFrame, y=None):
         """
@@ -205,11 +357,141 @@ class DataPreparation(TransformerMixin):
             if "calculated_distance" in dfc.columns:
                 dfc = dfc[~dfc["calculated_distance"].isna()]
 
-        dfc["measurement_data"] = dfc["measurement_data"].apply(
-            lambda x: np.nan_to_num(x)
+        if "age" in dfc.columns:
+            dfc["age"] = df["age"].fillna(-1)
+
+        if "measurement_data" in dfc.columns:
+            dfc["measurement_data"] = dfc["measurement_data"].apply(
+                lambda x: np.nan_to_num(x)
+            )
+
+        if "calculated_distance" in dfc.columns:
+            dfc["type_measurement"] = dfc["calculated_distance"].apply(
+                lambda d: "WAXS" if d < 0.05 else "SAXS"
+            )
+
+        if self.limits:
+            limits_waxs = (self.limits.q_min_waxs, self.limits.q_max_waxs)
+            limits_saxs = (self.limits.q_min_saxs, self.limits.q_max_saxs)
+            if "type_measurement" not in dfc.columns:
+                dfc["type_measurement"] = dfc[
+                    "calibration_manual_distance"
+                ].apply(lambda d: "WAXS" if d < 50 else "SAXS")
+            dfc["interpolation_q_range"] = dfc["type_measurement"].apply(
+                lambda x: limits_waxs if x == "WAXS" else limits_saxs
+            )
+
+        if self.cleaning_rules:
+            dfc = self._clean(dfc)
+        return dfc[self.columns]
+
+    def _clean(self, df: pd.DataFrame):
+        def clean(row, cleaning_rules: List[Rule]):
+            res = []
+            for r in cleaning_rules:
+                r: Rule = r
+                ret = False
+                idx = np.argmin(np.abs(row["q_range"] - r.q_value))
+                intensity = row["radial_profile_data"][idx]
+                if r.lower is not None and r.upper is not None:
+                    ret = (intensity > r.lower) and (intensity < r.upper)
+                elif r.lower is not None:
+                    ret = intensity > r.lower
+                elif r.upper is not None:
+                    ret = intensity < r.upper
+                res.append(ret)
+            return all(res)
+
+        if self.cleaning_rules:
+            return df[
+                df.apply(clean, axis=1, cleaning_rules=self.cleaning_rules)
+            ].copy()
+        else:
+            print("No cleaning was done, thera are no cleaning_rules")
+            return df
+
+
+class NormScaler(TransformerMixin):
+    """
+    Does normalization and scaling of the dataframe
+    """
+
+    def __init__(
+        self, scalers: Dict[str, StandardScaler] = None, name="Scaler"
+    ):
+        self._name = name
+        if scalers:
+            self.scalers = scalers
+        else:
+            self.scalers = {}
+
+    def fit(self, df: pd.DataFrame, y=None):
+        print(f"NormScaler {self._name}: is doing fit.")
+        dfc = df.copy()
+        norm = Normalizer("l1")
+        dfc["radial_profile_data_norm"] = dfc["radial_profile_data"].apply(
+            lambda x: norm.transform([x])[0]
         )
 
-        return dfc[self.columns]
+        df_saxs = dfc[dfc["type_measurement"] == "SAXS"].copy()
+        df_waxs = dfc[dfc["type_measurement"] == "WAXS"].copy()
+
+        if not df_saxs.empty:
+            scaler_saxs = StandardScaler()
+            matrix_2d_saxs = np.vstack(
+                df_saxs["radial_profile_data_norm"].values
+            )
+            scaler_saxs.fit(matrix_2d_saxs)
+            self.scalers["SAXS"] = scaler_saxs
+
+        # Apply the scaler for WAXS data
+        if not df_waxs.empty:
+            scaler_waxs = StandardScaler()
+            matrix_2d_waxs = np.vstack(
+                df_waxs["radial_profile_data_norm"].values
+            )
+            scaler_waxs.fit(matrix_2d_waxs)
+            self.scalers["WAXS"] = scaler_waxs
+
+        return self
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        print(f"NormScaler {self._name}: is doing transform.")
+        if not self.scalers:
+            self.fit(df)
+        dfc = df.copy()
+        raw_index = dfc.index
+        norm = Normalizer("l1")
+        dfc["radial_profile_data_norm"] = dfc["radial_profile_data"].apply(
+            lambda x: norm.transform([x])[0]
+        )
+
+        df_saxs = dfc[dfc["type_measurement"] == "SAXS"].copy()
+        df_waxs = dfc[dfc["type_measurement"] == "WAXS"].copy()
+
+        # Apply the scaler for SAXS data
+        if not df_saxs.empty:
+            matrix_2d_saxs = np.vstack(
+                df_saxs["radial_profile_data_norm"].values
+            )
+            scaled_data_saxs = self.scalers["SAXS"].transform(matrix_2d_saxs)
+            df_saxs["radial_profile_data_norm_scaled"] = [
+                arr for arr in scaled_data_saxs
+            ]
+
+        # Apply the scaler for WAXS data
+        if not df_waxs.empty:
+            matrix_2d_waxs = np.vstack(
+                df_waxs["radial_profile_data_norm"].values
+            )
+            scaled_data_waxs = self.scalers["WAXS"].transform(matrix_2d_waxs)
+            df_waxs["radial_profile_data_norm_scaled"] = [
+                arr for arr in scaled_data_waxs
+            ]
+
+        # Combine the processed DataFrames back into one
+        dfc_processed = pd.concat([df_saxs, df_waxs])
+        return dfc_processed.loc[raw_index]
 
 
 class Clusterization(TransformerMixin):
@@ -360,7 +642,9 @@ class InterpolatorClusters(TransformerMixin):
         clusters = {}
         for cluster_label in dfc["q_cluster_label"].unique():
             azimuth = AzimuthalIntegration(
-                faulty_pixels=self.faulty_pixel_array, npt=self.q_resolution
+                faulty_pixels=self.faulty_pixel_array,
+                npt=self.q_resolution,
+                calibration_mode="poni",
             )
             clusters[cluster_label] = interpolate_cluster(
                 dfc, cluster_label, self.perc_min, self.perc_max, azimuth
@@ -378,14 +662,14 @@ class NormScalerClusters(TransformerMixin):
     Transformer class for normalizing and scaling clusters of azimuthal
     integration data.
 
-    :param model_names: Names of the models.
-    :type model_names: List[str]
+    :param modelscales: Names of the models.
+    :type modelscales: List[str]
     :param do_fit: Whether to fit the scaler. Defaults to True.
     :type do_fit: bool
     """
 
-    def __init__(self, model_names: List[str], do_fit=True):
-        self.model_names = model_names
+    def __init__(self, modelscales: Dict[str, ModelScale], do_fit=True):
+        self.modelscales = modelscales
         self.do_fit = do_fit
 
     def fit(self, x):
@@ -414,7 +698,12 @@ class NormScalerClusters(TransformerMixin):
         :return: Dictionary containing normalized and scaled clusters.
         :rtype: dict
         """
-        for container in containers.values():
+        for name, container in containers.items():
             for cluster in container.clusters.values():
-                normalize_scale_cluster(cluster, do_fit=self.do_fit)
+                normalize_scale_cluster(
+                    cluster,
+                    normt=self.modelscales[name].normt,
+                    norm=self.modelscales[name].norm,
+                    do_fit=self.do_fit,
+                )
         return containers
