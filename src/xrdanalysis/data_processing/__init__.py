efd2f5213a4ad8864a9e87ad43d4984eb7324999
saldenisov <saldenisov@gmail.com>
2025-10-06 17:47:41 +0200

    chore: apply isort formatting after refactor (pre-commit)

--- src/xrdanalysis/data_processing/__init__.py
+++ src/xrdanalysis/data_processing/__init__.py
@@ -6,5 +6,6 @@ from .calibration_corrections import (
     compute_calib_correction_profiles,
     correct_with_calib_profiles,
 )
-from .detector_joining import DetectorJoiner, join_detectors
+from .detector_joining import join_detectors
 from .splitters import grouped_splitter
+from .transformers import DetectorJoiner
